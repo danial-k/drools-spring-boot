6b870eca4b8dd637f02008bcae12389a056539c3
Danial Khoshkhou <danial.k@me.com>
2019-06-14 07:53:06 +0100

    Added project example source

---
+++ spring-boot/src/main/java/com/example/springboot/RulesController.java
@@ -0,0 +1,38 @@
+package com.example.springboot;
+
+import com.example.drools.Measurement;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+@RestController
+public class RulesController {
+
+    //@Autowired
+    //private RuleService ruleService;
+
+    @RequestMapping("/rules")
+    public String rules(
+        @RequestParam(value="id", defaultValue="") String id,
+        @RequestParam(value="val", defaultValue="") String val
+    ) {
+        Measurement measurement = new Measurement(id, val);
+
+        // See https://docs.jboss.org/drools/release/7.18.0.Final/kie-api-javadoc/
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(measurement);
+        kieSession.fireAllRules();
+
+        return measurement.toString();
+    }
+}
+
+//<context:component-scan base-package="com.example.yourapp"/>
