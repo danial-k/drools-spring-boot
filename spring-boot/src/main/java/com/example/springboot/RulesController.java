a20d4b991921757e7c01f509e515fdb3f13b9511
Danial Khoshkhou <31406378+danial-k@users.noreply.github.com>
2019-06-18 12:49:26 +0100

    Update RulesController.java

--- spring-boot/src/main/java/com/example/springboot/RulesController.java
+++ spring-boot/src/main/java/com/example/springboot/RulesController.java
@@ -1,7 +1,6 @@
 package com.example.springboot;
 
 import com.example.drools.Measurement;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +12,6 @@ import org.kie.api.runtime.KieSession;
 
 @RestController
 public class RulesController {
-
-    //@Autowired
-    //private RuleService ruleService;
 
     @RequestMapping("/rules")
     public String rules(
